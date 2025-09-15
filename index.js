d7888801e572e8a42afe0cea3f3e2d1fb92760ae
Fathimathul-Huda <fathimathulhuda21@gmail.com>
2025-09-15 21:26:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const mongodb=require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config();
+const app=express();
+const port=3000;
+app.use(express.json())
+mongodb.connect(process.env.MONGO)
+.then(()=>{
+    console.log("db connected");
+})
+.catch((err)=>{
+    console.log("db connection error:",err);
+})
+const userdot=require("./Routes/taskRoutes")
+app.use("/",userdot)
+
+app.listen(port,()=>{
+    console.log(`server running at port ${port}`);
+})
